bfd6a269d0092f5ef7fe298ce43560670dc230d8
Anes-Zaidi <ryderanes66@gmail.com>
2026-04-10 15:09:39 +0100

    migrated to supabase

--- drizzle/0002_uneven_master_mold.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "participants" ADD CONSTRAINT "participants_email_unique" UNIQUE("email");--> statement-breakpoint
-ALTER TABLE "visitors" ADD CONSTRAINT "visitors_email_unique" UNIQUE("email");
